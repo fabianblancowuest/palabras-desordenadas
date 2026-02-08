b025fb347712f1704797023dee307679af8a1cb2
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2026-02-08 17:53:38 -0300

    update

--- src/components/ScrambleWords.tsx
+++ src/components/ScrambleWords.tsx
@@ -94,7 +94,7 @@ export const ScrambleWords = () => {
 					<h1 className="text-4xl font-bold bg-linear-to-r from-purple-600 to-blue-600 bg-clip-text text-transparent mb-2">
 						Palabras desordenadas
 					</h1>
-					<h2 className="text-3xl">DALE LUCERO, JUGÁ </h2>
+					<h2 className="text-3xl">DALE MAMER, JUGÁ </h2>
 					<p className="text-gray-600">
 						Desordena las letras para encontrar la palabra!
 					</p>
