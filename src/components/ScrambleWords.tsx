b1abf13adcaf57151a187df789305e98161fa654
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2026-03-05 01:00:57 -0300

    cambiar estilo título

--- src/components/ScrambleWords.tsx
+++ src/components/ScrambleWords.tsx
@@ -92,7 +92,7 @@ export const ScrambleWords = () => {
 				{/* Header */}
 				<div className="text-center mb-8">
 					<h1 className="text-4xl font-bold bg-linear-to-r from-purple-600 to-blue-600 bg-clip-text text-transparent mb-2">
-						Palabras Desordenadas
+						PALABRAS DESORDENADAS
 					</h1>
 					<p className="text-gray-600">
 						¡Desordená las letras para encontrar la palabra!
